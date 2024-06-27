1e39855d82fc6271746900fbcfa6e7e7d7d67f0b
Filipe Durães <77279496+filipeduraes@users.noreply.github.com>
2024-06-27 19:13:42 -0300

    Add conversion functions

--- Assets/Scripts/MapAPI/MapManager.cs
+++ Assets/Scripts/MapAPI/MapManager.cs
@@ -10,9 +10,9 @@ namespace Unicar.MapAPI
     public class MapManager : MonoBehaviour
     {
         private const string ApiUrl = "https://api.openstreetmap.org/";
-        [SerializeField] private string x;
-        [SerializeField] private string y;
-        [SerializeField] private string zoom;
+        [SerializeField] private double latitude;
+        [SerializeField] private double longitude;
+        [SerializeField, Range(0, 18)] private int zoom;
         [SerializeField] private RawImage rawImage;
 
         private CancellationTokenSource _cancellationTokenSource;
@@ -37,14 +37,39 @@ namespace Unicar.MapAPI
             
             client.DefaultRequestHeaders.Add("User-Agent", "Unicar / 0.1 development");
 
-            HttpResponseMessage response = await client.GetAsync($"http://tile.openstreetmap.org/{zoom}/{x}/{y}.png");
+            int tileX = ConvertLongitudeToTileX(longitude, zoom);
+            int tileY = ConvertLatitudeToTileY(latitude, zoom);
+            
+            HttpResponseMessage response = await client.GetAsync($"http://tile.openstreetmap.org/{zoom}/{tileX}/{tileY}.png");
             byte[] image = await response.Content.ReadAsByteArrayAsync();
 
-            Texture2D texture = new(256, 256); 
+            Texture2D texture = new(512, 512); 
             texture.LoadImage(image);
             texture.Apply();
 
             rawImage.texture = texture;
+        }
+
+        private int ConvertLongitudeToTileX(double longitude, int zoom)
+        {
+            return (int) Math.Round((longitude + 180.0) / 360.0 * (1 << zoom));
+        }
+
+        private int ConvertLatitudeToTileY(double latitude, int zoom)
+        {
+            double latRad = latitude / 180.0 * Math.PI;
+            return (int) Math.Floor((1 - Math.Log(Math.Tan(latRad) + 1 / Math.Cos(latRad)) / Math.PI) / 2 * (1 << zoom));
+        }
+
+        private double ConvertTileXToLongitude(int x, int zoom)
+        {
+            return (double) x / (1 << zoom) * 360.0 - 180.0;
+        }
+
+        private double ConvertTileYToLatitude(int y, int zoom)
+        {
+            double n = Math.PI - 2.0 * Math.PI * y / (1 << zoom);
+            return 180.0 / Math.PI * Math.Atan(0.5 * (Math.Exp(n) - Math.Exp(-n)));
         }
     }
 }
