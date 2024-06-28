9f34397d169c78f2a21de1e858590e27a8a35397
Filipe Durães <77279496+filipeduraes@users.noreply.github.com>
2024-06-28 13:56:52 -0300

    Cache map tiles and make populate tasks parallel

--- Assets/Scripts/MapAPI/MapManager.cs
+++ Assets/Scripts/MapAPI/MapManager.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Net.Http;
-using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,49 +12,78 @@ namespace Unicar.MapAPI
         
         private const string ApiUrl = "https://api.openstreetmap.org/";
         
-        public static async UniTask<Texture2D> GetMapFromCoordinate(int tileX, int tileY, int zoomLevel)
+        public static async UniTask<Texture2D> GetMapFromCoordinate(TilePoint tile, int zoomLevel)
         {
             using HttpClient client = new();
             client.DefaultRequestHeaders.Add("User-Agent", "Unicar / 0.1 development");
+
+            string requestUri = $"http://tile.openstreetmap.org/{zoomLevel}/{tile.x}/{tile.y}.png";
+            Debug.Log(requestUri);
             
-            HttpResponseMessage response = await client.GetAsync($"http://tile.openstreetmap.org/{zoomLevel}/{tileX}/{tileY}.png");
+            HttpResponseMessage response = await client.GetAsync(requestUri);
             byte[] image = await response.Content.ReadAsByteArrayAsync();
 
-            Texture2D texture = new(512, 512); 
+            Texture2D texture = new(256, 256); 
             texture.LoadImage(image);
             texture.Apply();
 
             return texture;
         }
-
-        public static Vector2Int ConvertCoordinateToTile(double latitude, double longitude, int zoomLevel)
+        
+        public static TilePoint ConvertCoordinateToTile(double latitude, double longitude, int zoomLevel)
         {
-            int tileX = ConvertLongitudeToTileX(longitude, zoomLevel);
-            int tileY = ConvertLatitudeToTileY(latitude, zoomLevel);
+            long tileX = ConvertLongitudeToTileX(longitude, zoomLevel);
+            long tileY = ConvertLatitudeToTileY(latitude, zoomLevel);
 
-            return new Vector2Int(tileX, tileY);
+            return new TilePoint(tileX, tileY, zoomLevel);
         }
 
-        public static int ConvertLongitudeToTileX(double longitude, int zoom)
+        public static long ConvertLongitudeToTileX(double longitude, int zoom)
         {
-            return (int) Math.Round((longitude + 180.0) / 360.0 * (1 << zoom));
+            return (long) Math.Floor((longitude + 180.0) / 360.0 * (1 << zoom));
         }
 
-        public static int ConvertLatitudeToTileY(double latitude, int zoom)
+        public static long ConvertLatitudeToTileY(double latitude, int zoom)
         {
-            double latRad = latitude / 180.0 * Math.PI;
-            return (int) Math.Floor((1 - Math.Log(Math.Tan(latRad) + 1 / Math.Cos(latRad)) / Math.PI) / 2 * (1 << zoom));
+            double latitudeRadian = latitude * Math.PI / 180.0;
+            long tileY = (long) Math.Floor((1 - Math.Log(Math.Tan(latitudeRadian) + 1 / Math.Cos(latitudeRadian)) / Math.PI) / 2 * (1 << zoom));
+            
+            return tileY;
         }
 
-        public static double ConvertTileXToLongitude(int x, int zoom)
+        public static double ConvertTileXToLongitude(long x, int zoom)
         {
             return (double) x / (1 << zoom) * 360.0 - 180.0;
         }
 
-        public static double ConvertTileYToLatitude(int y, int zoom)
+        public static double ConvertTileYToLatitude(long y, int zoom)
         {
             double n = Math.PI - 2.0 * Math.PI * y / (1 << zoom);
             return 180.0 / Math.PI * Math.Atan(0.5 * (Math.Exp(n) - Math.Exp(-n)));
+        }
+    }
+
+    public struct TilePoint
+    {
+        public readonly long x;
+        public readonly long y;
+        public readonly int zoom;
+
+        public TilePoint(long x, long y, int zoom)
+        {
+            this.x = x;
+            this.y = y;
+            this.zoom = zoom;
+        }
+
+        public static TilePoint operator +(TilePoint aTilePoint, TilePoint bTilePoint)
+        {
+            return new TilePoint(aTilePoint.x + bTilePoint.x, aTilePoint.y + bTilePoint.y, aTilePoint.zoom);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(zoom, x, y);
         }
     }
 }
--- Assets/Scripts/UI/RidesScreen/View/MapDrawer.cs
+++ Assets/Scripts/UI/RidesScreen/View/MapDrawer.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using Unicar.MapAPI;
 using UnityEngine;
@@ -9,13 +11,14 @@ namespace Unicar.UI.RidesScreen.View
     public class MapDrawer : MonoBehaviour
     {
         [SerializeField] private RectTransform mapCenter;
-        [SerializeField] private double longitude;
         [SerializeField] private double latitude;
+        [SerializeField] private double longitude;
         [SerializeField] private int zoomLevel = 10;
-        
+
+        private Dictionary<TilePoint, Texture2D> _cacheTextures = new();
         private readonly RawImage[,] _tileImages = new RawImage[3, 3];
         private CancellationTokenSource _cancellationTokenSource;
-        private Vector2Int _initialTile;
+        private TilePoint _initialTile;
 
         private void Awake()
         {
@@ -35,8 +38,11 @@ namespace Unicar.UI.RidesScreen.View
 
         private async UniTask PopulateMapTexturesAsync()
         {
+            UniTask[] populateTasks = new UniTask[_tileImages.GetLength(0) * _tileImages.GetLength(1)];
             _initialTile = MapManager.ConvertCoordinateToTile(latitude, longitude, zoomLevel);
-            int y = -1;
+
+            int taskIndex = 0;
+            int y = 1;
 
             for (int j = 0; j < _tileImages.GetLength(1); j++)
             {
@@ -44,23 +50,43 @@ namespace Unicar.UI.RidesScreen.View
                 
                 for (int i = 0; i < _tileImages.GetLength(0); i++)
                 {
-                    Texture2D map = await MapManager.GetMapFromCoordinate(_initialTile.x + x, _initialTile.y + y, zoomLevel);
-                    _tileImages[i, j].texture = map;
+                    TilePoint tile = _initialTile + new TilePoint(x, y, _initialTile.zoom);
+                    populateTasks[taskIndex] = PopulateTile(tile, new Vector2Int(i, j));
+                    
                     x++;
+                    taskIndex++;
                 }
 
-                y++;
+                y--;
             }
+
+            await UniTask.WhenAll(populateTasks);
+        }
+
+        private async UniTask PopulateTile(TilePoint tilePoint, Vector2Int index)
+        {
+            Texture2D map = await SpawnAndCacheMapTileAsync(tilePoint);
+            _tileImages[index.x, index.y].texture = map;
+        }
+
+        private async UniTask<Texture2D> SpawnAndCacheMapTileAsync(TilePoint tilePoint)
+        {
+            if (_cacheTextures.TryGetValue(tilePoint, out Texture2D mapTile))
+                return mapTile;
+            
+            Texture2D map = await MapManager.GetMapFromCoordinate(tilePoint, zoomLevel);
+            _cacheTextures[tilePoint] = map;
+            return map;
         }
 
         private void GenerateTileImages()
         {
-            int initialX = -1;
-            Vector2 size = mapCenter.rect.size * 2f;
+            int initialY = -1;
+            float size = mapCenter.rect.width * 2f;
             
             for (int j = 0; j < _tileImages.GetLength(1); j++)
             {
-                int initialY = -1;
+                int initialX = -1;
                 
                 for (int i = 0; i < _tileImages.GetLength(0); i++)
                 {
@@ -68,16 +94,16 @@ namespace Unicar.UI.RidesScreen.View
                     rawImage.transform.SetParent(transform);
 
                     Vector2 position = (Vector2) mapCenter.position + size * new Vector2(initialX, initialY);
-                    rawImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size.x);
-                    rawImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size.y);
+                    rawImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size);
+                    rawImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size);
 
                     rawImage.rectTransform.position = position;
 
                     _tileImages[i, j] = rawImage;
-                    initialY++;
+                    initialX++;
                 }
 
-                initialX++;
+                initialY++;
             }
         }
     }
